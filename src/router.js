a9c3f97fd5c0fcd9386c84baabe614589f89a23c
Turkuvaz <Turkuvaz@TRKVZ-21502>
2021-10-05 17:36:27 +0300

    Initial commit

---
+++ src/router.js
@@ -0,0 +1,106 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Dashboard from './components/Dashboard.vue';
+
+const routes = [
+    {
+        path: '/',
+        name: 'dashboard',
+        component: Dashboard,
+    },
+    {
+        path: '/formlayout',
+        name: 'formlayout',component: Dashboard,
+    },
+    {
+        path: '/input',
+        name: 'input',component: Dashboard,
+    },
+    {
+        path: '/floatlabel',
+        name: 'floatlabel',component: Dashboard,
+    },
+    {
+        path: '/invalidstate',
+        name: 'invalidstate',component: Dashboard,
+    },
+    {
+        path: '/button',
+        name: 'button',component: Dashboard,
+    },
+    {
+        path: '/table',
+        name: 'table',component: Dashboard,
+    },
+    {
+        path: '/list',
+        name: 'list',component: Dashboard,
+    },
+    {
+        path: '/tree',
+        name: 'tree',component: Dashboard,
+    },
+    {
+        path: '/panel',
+        name: 'panel',component: Dashboard,
+    },
+    {
+        path: '/overlay',
+        name: 'overlay',component: Dashboard,
+    },
+    {
+        path: '/menu',component: Dashboard,
+        children: [
+            {
+                path: '',component: Dashboard,
+            },
+            {
+                path: '/menu/seat',component: Dashboard,
+            },
+            {
+                path: '/menu/payment',component: Dashboard,
+            },
+            {
+                path: '/menu/confirmation',component: Dashboard,
+            },
+        ],
+    },
+    {
+        path: '/messages',
+        name: 'messages',component: Dashboard,
+    },
+    {
+        path: '/file',
+        name: 'file',component: Dashboard,
+    },
+    {
+        path: '/chart',
+        name: 'chart',component: Dashboard,
+    },
+    {
+        path: '/misc',
+        name: 'misc',component: Dashboard,
+    },
+    {
+        path: '/crud',
+        name: 'crud',component: Dashboard,
+    },
+    {
+        path: '/timeline',
+        name: 'timeline',component: Dashboard,
+    },
+    {
+        path: '/empty',
+        name: 'empty',component: Dashboard,
+    },
+    {
+        path: '/documentation',
+        name: 'documentation',component: Dashboard,
+    },
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
+
+export default router;
