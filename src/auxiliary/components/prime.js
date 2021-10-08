315c8b9bbac5d7fddee79ec70578dfb49300298b
Turkuvaz <Muhammedzturk>
2021-10-08 18:29:17 +0300

    Offer Create Tab kısmı düzenlendi.

--- src/auxiliary/components/prime.js
+++ src/auxiliary/components/prime.js
@@ -6,6 +6,13 @@ import TabPanel from 'primevue/tabpanel';
 import InputText from 'primevue/inputtext';
 import Dialog from 'primevue/dialog';
 import Calendar from 'primevue/calendar';
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import Textarea from 'primevue/textarea';
+import Editor from 'primevue/editor';
+import SplitButton from 'primevue/splitbutton';
+
+
 export const prime = (app) => {
 
     app.component('Button',Button);
@@ -16,4 +23,9 @@ export const prime = (app) => {
     app.component('InputSwitch',InputSwitch);
     app.component('Dialog',Dialog);
     app.component('Calendar',Calendar);
+    app.component('DataTable',DataTable);
+    app.component('Column',Column);
+    app.component('Textarea',Textarea);
+    app.component('Editor',Editor);
+    app.component('SplitButton',SplitButton);
 }
--- src/view/pages/Offer/OfferCreate.vue
+++ src/view/pages/Offer/OfferCreate.vue
@@ -4,7 +4,7 @@
       <div class=" p-md-6 p-fluid">
         <h6>Yeni Teklif Oluştur</h6>
         <div class="card">
-          <small class="ml-2">Teklif Numarası </small>
+          <small class="ml-2">Teklif Numarası</small>
           <div class="grid formgrid col-12">
 
             <div class="col-2 mb-4 ">
@@ -23,50 +23,126 @@
               <label class=" col" for="offerdate">Teklif Tarihi</label>
             </div>
             <div class="col-4">
-              <Calendar id="offerdate"  class="p-lg-4" v-model="value" :showButtonBar="true" />
+              <Calendar id="offerdate"  class="p-lg-4" v-model="value" :showButtonBar="true" :showIcon="true" />
             </div>
             </div>
           </div>
         <div class="card">
-          <h5>Icons</h5>
+          <h5>Teklif Ekle</h5>
           <div class="p-grid p-formgrid">
-            <div class="p-col-12 p-mb-2 p-lg-4 p-mb-lg-0">
-                            <span class="p-input-icon-left">
-                                <i class="pi pi-user" />
-                                <InputText type="text" placeholder="Username" />
-                            </span>
-            </div>
-            <div class="p-col-12 p-mb-2 p-lg-4 p-mb-lg-0">
-                            <span class="p-input-icon-right">
-                                <InputText type="text" placeholder="Search" />
-                                <i class="pi pi-search" />
-                            </span>
-            </div>
-            <div class="p-col-12 p-mb-2 p-lg-4 p-mb-lg-0">
-                            <span class="p-input-icon-left p-input-icon-right">
-                                <i class="pi pi-user" />
-                                <InputText type="text" placeholder="Search" />
-                                <i class="pi pi-search" />
-                            </span>
-            </div>
+            <TabView>
+              <TabPanel header="Teklif Bilgisi">
+                <div  class="card">
+                  <div class="col-4">
+                    <span class="p-buttonset">
+                      <Button style="border-radius: 6px; background-color: green" class="mr-1"  label="Kaydet" icon="pi pi-check" />
+                      <Button style="border-radius: 6px; background-color: dodgerblue"  class="mr-1" label="Güncelle" icon="pi pi-refresh" />
+                      <Button style="border-radius: 6px; background-color: darkred" label="Sil" icon=" pi pi-trash" />
+                    </span>
+
+                  </div>
+                  <div>
+                    <DataTable >
+                      <Column field="rowNumber" header="Sıra No"></Column>
+                      <Column field="offerDefinition" header="Teklif Tanımı"></Column>
+                      <Column field="currency" header="Para Birimi"></Column>
+                      <Column field="unitPrice" header="Birim Fiyat"></Column>
+                      <Column field="profitRate" header="Kar Oranı"></Column>
+                      <Column field="quantity" header="Miktar"></Column>
+                      <Column field="totalprice" header="Toplam Fiyat"></Column>
+                    </DataTable>
+                  </div>
+                </div>
+
+              </TabPanel>
+              <TabPanel header="Ticari Koşullar">
+                <div class="card">
+                <div>
+                <Editor v-model="value3" editorStyle="height: 320px" placeholder="Ticari Koşullar"/>
+                </div>
+                <div class="col-2 ml-auto mt-1 ">
+                  <SplitButton label="Save" icon="pi pi-plus" :model="items"></SplitButton>
+                </div>
+                </div>
+              </TabPanel>
+
+              <TabPanel header=" Teklif Süre Bilgisi">
+                <div class="card">
+                <div>
+                <Editor v-model="value1" editorStyle="height: 320px" placeholder="Teklif Süre Bilgisi"/>
+                </div>
+                <div class="col-2 ml-auto mt-1 ">
+                <SplitButton label="Save" icon="pi pi-plus" :model="items"></SplitButton>
+                </div>
+                </div>
+              </TabPanel>
+
+              <TabPanel header="Teklif Açıklamaları">
+                <div class="card">
+                <div>
+                <Editor v-model="value2" editorStyle="height: 320px" placeholder="Teklif Açıklamaları"/>
+                </div>
+                <div class="col-2 ml-auto mt-1 ">
+                <SplitButton label="Save" icon="pi pi-plus" :model="items"></SplitButton>
+                </div>
+              </div>
+              </TabPanel>
+            </TabView>
+
           </div>
 </div>
-          <h5>Float Label</h5>
-          <span class="p-float-label">
-                        <InputText id="username" type="text" v-model="floatValue"/>
-                        <label for="username">Username</label>
-                    </span>
-        </div>
 
-        <div class="card">
-          <h5>Slider</h5>
-          <InputText v-model.number="sliderValue" />
-          <Slider v-model="sliderValue" />
         </div>
       </div>
     </div>
 
 </template>
+<script>
+import { ref } from 'vue';
+export default {
+  data() {
+    return {
+      products: null,
+      commercialTerms:'',
+      offerTimeInformation:'',
+      offerDescriptions:'',
+      paymentCondition:'',
+      deliveryTime:'',
+      offerValidityDate:'',
+      offerDescription:'',
+      offerNotes:'',
+      offerContract:'',
+      offerCoverLetter:'',
+
+      items: [
+        {
+          label: 'Update',
+          icon: 'pi pi-refresh',
+          command: () => {
+            this.$toast.add({severity:'success', summary:'Updated', detail:'Data Updated', life: 3000});
+          }
+        },
+        {
+          label: 'Delete',
+          icon: 'pi pi-times',
+          command: () => {
+            this.$toast.add({ severity: 'warn', summary: 'Delete', detail: 'Data Deleted', life: 3000});
+          }
+        },
+      ]
+    };
+
+  },
+    setup() {
+      const value1 = ref('');
+      const value2 = ref('');
+      const value3 = ref('');
+      return {value1,value2,value3 };
+    },
+
+
+}
+</script>
 <style scoped>
 </style>
 
